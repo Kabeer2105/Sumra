b8b6699c3fe589f52e768791f26c35afe10a131c
Kabeer Dalvi <kabeer.d@gmail.com>
2025-05-11 07:44:48 +0200

    again

--- backend/index.js
+++ backend/index.js
@@ -50,22 +50,34 @@ app.listen(PORT, () => {
 
 require('dotenv').config();
 
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
 
 const app = express();
 const PORT = 3000;
 
-const mongoURI = process.env.MONGODB_URI;
 
 // Replace this with your actual MongoDB Atlas connection string
-mongoose.connect(mongoURI, { dbName: 'citydata' })
+mongoose.connect('mongodb+srv://kabeerd:Ashifa2505@cluster0.yke9w5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { dbName: 'citydata' })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('Mongo error:', err));
 
 app.use(cors());
 app.use(express.json());
+
+// Serve static files from the frontend folder
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Fallback for the root route to serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
 
 const personSchema = new mongoose.Schema({
   name: String,
